c681a175703ea5f57e72cf8cb19f30ae29a19ce0
rk-madrisan <rk.riyas@outlook.com>
2024-12-11 14:42:42 +0530

    added

---
+++ dictionary.py
@@ -0,0 +1,16 @@
+
+person = {
+    "name": "Alice",
+    "age": 25,
+    "city": "New York"
+}
+
+
+print("Original dictionary:", person)
+print("Name:", person["name"])
+person["age"] += 1
+print("Updated age:", person["age"])
+
+
+person["job"] = "Developer"
+print("Updated dictionary:", person)
