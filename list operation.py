0e9855a6ba075afc0213844ced415fa7b4855780
rk-madrisan <rk.riyas@outlook.com>
2024-12-11 14:35:21 +0530

    added

---
+++ list operation.py
@@ -0,0 +1,9 @@
+
+numbers = [10, 20, 30, 40, 50]
+
+
+print("Original list:", numbers)
+print("Reversed list:", numbers[::-1])
+print("Sum of all elements:", sum(numbers))
+print("Largest element:", max(numbers))
+print("Smallest element:", min(numbers))
